139eb4e7d13b1f609a41f6d83558550bd5354ac2
Izequias Silva <silvaizequias@icloud.com>
2023-12-05 15:44:45 -0300

    top bar element

---
+++ src/app/elements/top-bar-element/top-bar-element.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-top-bar-element',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-bar-element.component.html',
+  styleUrl: './top-bar-element.component.css',
+})
+export class TopBarElementComponent {}
